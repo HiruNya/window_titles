7a64940caf04c61001569f287959d8dac54c4d37
Hiruna Jayamanne <hiru@hiru.dev>
2021-05-15 15:15:00 +1200

    Expose windows module

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@ pub use connection::Connection;
 #[cfg_attr(target_os = "macos", path = "apple.rs")]
 mod connection;
 
+#[cfg_attr(target_os = "windows", path = "winapi.rs")]
+pub mod windows;
+
 pub type Result<T> = StdResult<T, Box<dyn Error>>;
 
 pub trait ConnectionTrait: Sized {
